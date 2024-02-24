e19af99fd64249adb66cda0ee966a96ce91d34b6
braydensawyer <bsawyer@pugetsound.edu>
2024-02-24 18:34:54 +0000

    hw4 final

--- dsh.c
+++ dsh.c
@@ -1,11 +1,11 @@
 /*
  * dsh.c
  *
- *  Created on: Aug 2, 2013
- *      Author: chiu
+ *  Created on: Feb 23, 2024
+ *      Author: Brayden Sawyer
  */
 #include "dsh.h"
-
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -20,9 +20,61 @@
 // TODO: Your function definitions below (declarations in dsh.h)
 
 /**
- * This is just an example. Delete this before 
- * submission.
+ * isEmpty function to check if a string is empty
  */
-void example(int* x) {
-    *x = thisIsGlobal;
+int isEmpty(const char *s) {
+    while(*s != '\0') {
+        if(!isspace((unsigned char) *s)) {
+            return 0;
+        }
+        s++;
+    }
+    return 1;
 } 
+
+/**
+ * checkLast function to see if the last element is '&'
+ */
+int checkLast(char** e) {
+    int i = 0;
+    while(e[i] != NULL) {
+        i++;
+    }
+    if(strcmp(e[i-1], "&") == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+//splits string into array based off a delim
+char** split(char* s, char* delim){
+    if(isEmpty(s)){
+        return NULL;
+    }
+    //find length of array
+    int len = 1; 
+    for(int i = 0; i < strlen(s); i++){
+        if(s[i] == *delim){
+            len++;
+        }
+    }
+    char **tokens = (char**)malloc((len + 1) * sizeof(char*));
+    //malloc all char*s inside tokens
+    for (int i = 0; i < len; i++) {
+        tokens[i] = (char*) malloc(128 * sizeof(char));
+    }
+
+    // get the first token 
+    char *token = strtok(s, delim);
+   
+    int i = 0; 
+    // walk through other tokens 
+    while( token != NULL) {
+        strcpy(tokens[i], token);
+        token = strtok(NULL, delim);
+        i++;
+    }
+    //NULL terminate last element
+    tokens[i] = NULL;
+    return tokens; 
+}
--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /*
  * main.c
  *
- *  Created on: Mar 17 2017
- *      Author: david
+ *  Created on: Feb 23 2024
+ *      Author: Brayden Sawyer
  */
 
 #include <stdio.h>
@@ -10,12 +10,13 @@
 #include <unistd.h>
 #include <sys/resource.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <string.h>
 #include "dsh.h"
 
 #define MAX_PROC 250
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
 
 	// DO NOT REMOVE THE BLOCK BELOW (FORK BOMB PREVENTION) //
 	struct rlimit limit;
@@ -25,7 +26,124 @@ int main(int argc, char *argv[]) {
 	// DO NOT REMOVE THE BLOCK ABOVE THIS LINE //
 
 
-	char *cmdline = (char*) malloc(MAXBUF); // stores user input from commmand line
+	char* cmd = (char*)malloc(MAXBUF); // stores user input from commmand line
+	int exit = 0;
 
+	while(exit == 0) {
+		//prompts user for input
+		printf("dsh> ");
+		fgets(cmd, MAXBUF, stdin);
+			//deletes '\n'
+			for(int i = 0; i < strlen(cmd); i++) {
+				if(cmd[i] == '\n') {
+					cmd[i] = '\0';
+				}
+			}
+		
+		
+		//creates array from user input
+		char** elements = split(cmd, " ");
+		//Check if input is empty
+		if(elements == NULL) {
+			printf("String can not be empty\n");
+		}
+		//exit
+		else if(strcmp(cmd, "exit") == 0) {
+			exit = 1;
+		}
+		//get current directory
+		else if(strcmp(elements[0], "pwd") == 0) {
+			char* curDir = malloc(MAXBUF);
+			getcwd(curDir, MAXBUF);
+			printf("%s\n", curDir);
+			free(curDir);
+		}
+		//change directory
+		else if(strcmp(elements[0], "cd") == 0){
+			//check if user gives you directory or not
+			if(elements[1] == NULL){
+				chdir(getenv("HOME"));
+			}
+			else{
+				chdir(elements[1]);
+			}
+		}
+		//mode 1
+		else if(elements[0][0] == '/'){
+			//check if file is valid
+			if (access(elements[0], F_OK | X_OK) == 0) {
+				//parent process
+				if(0 != fork()){
+					//only waits if last element is not &
+					if(checkLast(elements) == 0){
+						wait(NULL);
+					}
+				}
+				//child process
+				else{
+					execv(elements[0], elements);
+				}
+			}
+			else {
+				printf("ERROR: %s not found!\n", elements[0]);
+			}
+
+		}
+		//mode 2
+		else {
+			char* curDir = malloc(MAXBUF);
+			//gets path 
+			char** paths = split(getenv("PATH"), ":");
+			int found = 0;
+			int i = 0;
+			//append user input to current directory
+			getcwd(curDir, MAXBUF);
+			strcat(curDir, "/");
+			strcat(curDir, elements[0]);
+			//loop paths from variable and make sure they are valid
+			while(found == 0 && paths[i] != NULL) {
+				//is file valid
+				if(access(curDir, F_OK | X_OK) == 0) {
+					found = 1;
+					//parent
+					if(0 != fork()) {
+						//waits if last element is not '&'
+						if(checkLast(elements) == 0) {
+							wait(NULL);
+						}
+					}
+					//child
+					else {
+						elements[0] = curDir;
+						execv(curDir, elements);
+					}
+				}
+				//append input to path
+				strcpy(curDir, paths[i]);
+			 	strcat(curDir, "/");
+			 	strcat(curDir, elements[0]);
+				i++;
+			}
+			if(found == 0) {
+				printf("ERROR: %s not found!\n", elements[0]);
+			}
+			free(curDir);
+			int j = 0;
+			while(paths[j] != NULL) {
+				free(paths[j]);
+				paths[j] = NULL;
+				j++;
+			}
+			free(paths);
+		}
+		int i = 0;
+		while(elements[i] != NULL) {
+			free(elements[i]);
+			elements[i] = NULL;
+			i++;
+		}
+		free(elements);
+	}
+	free(cmd);
 	return 0;
 }
